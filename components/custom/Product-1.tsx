2dfa4900efe55281894bde20c01c94904b08f495
RayRemnant <rayremnant.me@gmail.com>
2024-02-22 16:49:44 +0000

    visual improvement for UX

--- components/custom/Product-1.tsx
+++ components/custom/Product-1.tsx
@@ -19,18 +19,26 @@ const Product1 = ({ name, imageUrl, amazonUrl, children }) => {
   return (
     <>
       <h3>{name}</h3>
-      <img
-        src={imageUrl || `https://images.amazon.com/images/P/${asin}._PI_MZZZZZZZ_.jpg`}
-        alt={name}
-        className="mx-auto"
-        width="500"
-        height="500"
-      />
+      <div className="flex flex-col md:flex-row xl:justify-evenly 2xl:justify-evenly gap-x-4 xl:gap-0 ">
+        <div>
+          <img
+            src={imageUrl || `https://images.amazon.com/images/P/${asin}._PI_MZZZZZZZ_.jpg`}
+            alt={name}
+            className="mx-auto"
+            width="250"
+            height="500"
+          />
+          <a href={amazonUrl} target="_blank">
+            <p className="m-auto w-fit whitespace-nowrap rounded-md border border-white p-3 ">Compra su Amazon</p>
+          </a>
+        </div>
+        <div className="h-auto w-px bg-gray-700"></div>
+
+        <div className="flex justify-center">
+          <img src={`https://graph.keepa.com/pricehistory.png?asin=${asin}&domain=${locale}`} className="self-center" />
+        </div>
+      </div>
       {children}
-      <a href={amazonUrl} target="_blank">
-        <p className="m-auto w-fit whitespace-nowrap rounded-md border border-white p-3 ">Compra su Amazon</p>
-      </a>
-      <img src={`https://graph.keepa.com/pricehistory.png?asin=${asin}&domain=${locale}`} className="mx-auto" />
     </>
   )
 }
