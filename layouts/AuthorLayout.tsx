d4bfb78a911e13de0bc5dd5b6349dbd0b5971fb0
RayRemnant <rayremnant.me@gmail.com>
2024-02-26 17:52:46 +0000

    adapting author page to my liking

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -26,14 +26,22 @@ export default async function AuthorLayout({ children, content, params: { locale
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center space-x-2 pt-8">
-            {avatar && <Image src={avatar} alt="avatar" width={192} height={192} className="h-48 w-48 rounded-full" />}
+            {avatar && (
+              <Image
+                src={avatar}
+                alt="avatar"
+                width={192}
+                height={192}
+                className="h-48 w-48 rounded-full border-2 border-white"
+              />
+            )}
             <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
+              {/* <SocialIcon kind="twitter" href={twitter} /> */}
             </div>
           </div>
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">{children}</div>
